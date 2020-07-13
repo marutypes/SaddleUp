924aa71b0de5874c19808597f66b66132799f343
Mathew  Allen <mathew.allen@shopify.com>
2020-07-13 10:43:59 -0400

    💅 make tsc setup more sewing-kitful

--- sewing-kit.config.ts
+++ sewing-kit.config.ts
@@ -1,6 +1,6 @@
 import {createPackage, Runtime} from '@sewing-kit/config';
 import {javascript} from '@sewing-kit/plugin-javascript';
-import {typescript} from '@sewing-kit/plugin-typescript';
+import {typescript, workspaceTypeScript} from '@sewing-kit/plugin-typescript';
 import {eslint} from '@sewing-kit/plugin-eslint';
 import {jest} from '@sewing-kit/plugin-jest';
 import {buildFlexibleOutputs} from '@sewing-kit/plugin-package-flexible-outputs';
@@ -19,5 +19,12 @@ export default createPackage((pkg) => {
     name: 'koa-matchers',
     root: './src/matchers/koa',
   });
-  pkg.use(buildFlexibleOutputs(), jest(), eslint(), javascript(), typescript());
+  pkg.use(
+    buildFlexibleOutputs(),
+    jest(),
+    eslint(),
+    javascript(),
+    typescript(),
+    workspaceTypeScript(),
+  );
 });
